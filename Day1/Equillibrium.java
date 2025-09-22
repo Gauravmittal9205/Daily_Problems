96626675c693a4b834d81f6f865b6abab8b9bfb5
Gauravmittal9205 <gauravmittal939@gmail.com>
2025-09-22 23:05:12 +0530

    Day1

---
+++ Day1/Equillibrium.java
@@ -0,0 +1,38 @@
+package Day1;
+
+import java.util.*;
+
+public class Equillibrium {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findEquilibrium(arr));
+        sc.close();
+
+    }
+
+    public static int findEquilibrium(int arr[]) {
+        int[] left = new int[arr.length];
+        int[] right = new int[arr.length];
+        int n = arr.length;
+        left[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            left[i] = left[i - 1] + arr[i];
+        }
+        right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] + arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            if (left[i] == right[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
