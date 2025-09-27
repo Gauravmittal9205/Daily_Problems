649d1c3205e987cd29b996cf4a579ff8d6a209bb
Gauravmittal9205 <gauravmittal939@gmail.com>
2025-09-27 23:48:46 +0530

    Sorting Algorithms

--- Day1/ICP_Class_Work/First_missing_number.java
+++ Day1/ICP_Class_Work/First_missing_number.java
@@ -5,12 +5,12 @@ import java.util.*;
 public class First_missing_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
-        // int nums[] = new int[n];
-        // for (int i = 0; i < n; i++) {
-        // nums[i] = sc.nextInt();
-        // }
-        // System.out.println(firstMissingPositive(nums));
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(firstMissingPositive(nums));
 
         sc.close();
     }
